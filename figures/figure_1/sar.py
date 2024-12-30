2216a498a71fb6954ebdc3f021c33cf511067475
Victor Boussange <vic.boussange@gmail.com>
2024-12-30 11:24:52 +0100

    added conceptual figure

--- figures/figure_1/sar.py
+++ figures/figure_1/sar.py
@@ -1,64 +1,82 @@
 # plotting expected SAR and dSAR, with sampling area dependent variation due to climate
-# TOFIX: problem with the derivative plot
+# TOFIX: problem with the derivative plot with autodiff, using finite difference for now
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+
+plt.rcParams.update({'axes.labelsize': 14})
 
 class ScalarFunctionModel(nn.Module):
 
     def forward(self, x):
         # return  1 - 0.5 * torch.exp(-x) - 0.5 * torch.exp(-x / 1000)
         # return  1 - 0.5 * torch.exp(-x / 1000)
-        return 1 / (1 + torch.exp(-x)) + 2 / (1 + torch.exp(-(x - 1000) / 1000))
+        return torch.log(1 / (1 + torch.exp(-x)) + 2 / (1 + torch.exp(-(x - 1000) / 1000)))
 
 def plot_model_output_and_derivative(model, x, y, y_derivative, std):
-    fig, axes = plt.subplots(2, 1, figsize=(6, 8), sharex=True)
+    fig, axes = plt.subplots(2, 1, figsize=(4, 6), sharex=True)
 
     # Plot the output value with synthetic standard deviation
     ax = axes[0]
-    ax.plot(x.detach().numpy(), y.detach().numpy(), label='Model Output')
+    ax.plot(x.detach().numpy(), y.detach().numpy())
     ax.fill_between(x.detach().numpy().flatten(), 
-                    (y - std).detach().numpy().flatten(), 
-                    (y + std).detach().numpy().flatten(), 
-                    color='blue', alpha=0.2, label='Synthetic Std Dev')
-    ax.set_xlabel('Area')
-    ax.set_ylabel('Species richness')
+                    (y * torch.exp(std)).detach().numpy().flatten(), 
+                    (y / torch.exp(std)).detach().numpy().flatten(), 
+                    color='blue', alpha=0.2)
+    ax.set_ylabel('Species richness (SR)')
     ax.set_yscale("log")
     ax.set_xscale("log")
-    ax.legend()
 
     # Plot the derivative with synthetic standard deviation
     ax = axes[1]
-    ax.plot(x.detach().numpy(), y_derivative.detach().numpy(), label='Model Derivative', color='r')
-    # ax.fill_between(x.detach().numpy().flatten(), 
-    #                 (y_derivative - std).detach().numpy().flatten(), 
-    #                 (y_derivative + std).detach().numpy().flatten(), 
-    #                 color='red', alpha=0.2, label='Synthetic Std Dev')
-    ax.set_xlabel('x')
-    ax.set_ylabel('d log Species richness / d log Area')
-    ax.set_xscale("log")
-    ax.set_yscale("log")
+    ax.plot(x.detach().numpy(), y_derivative.detach().numpy().flatten(), color='r')
+    ax.fill_between(x.detach().numpy().flatten(), 
+                    (y_derivative + 1e-1 * std).detach().numpy().flatten(), 
+                    (y_derivative - 1e-1 * std).detach().numpy().flatten(), 
+                    color='red', alpha=0.2)
+    ax.set_ylabel(r"$\frac{d \log(SR)}{d \log(A)}$", fontsize=18)
+    # ax.set_xscale("log")
+    # ax.set_yscale("log")
     # ax.set_title('Model Derivative vs x')
-    ax.legend()
+    ax.set_xlabel('Area')
 
-    plt.tight_layout()
-    plt.show()
+    fig.tight_layout()
+    return fig, axes
 
-# Example usage
 model = ScalarFunctionModel()
-
-# Generate data
 steps=100
-# x = torch.linspace(0, 100, steps).unsqueeze(1)
+
+# forward pass calculation
 x = torch.logspace(-2, 4, steps, base=10).unsqueeze(1)
+x.requires_grad_(True)
 y = model(x)
 
-# Compute the derivative
-x.requires_grad_(True)
-y_derivative = torch.autograd.grad(outputs=model(x).sum(), inputs=x)[0]
+# backward pass calculation, autodiff - not working
+# x = torch.linspace(-2, 4, steps).unsqueeze(1)
+# x = torch.pow(10, x)
+# y_derivative = torch.autograd.grad(outputs=y, inputs=x, grad_outputs=torch.ones_like(y))[0]
 
-# Create synthetic standard deviation that decreases with increasing x
-std = 1e-1 * torch.exp(0.2 / (x * 1e-2 + 1))
+# backward pass calculation, finite difference
+dx = x[1] - x[0]
+y_finite_diff = (y[2:] - y[:-2]) / (2 * dx) / 30
+x_finite_diff = x[1:-1]
+y = y[1:-1]
+x = x[1:-1]
+# plt.plot(x, y_finite_diff)
+# plt.xscale("log")
+
+# Synthetic standard deviation
+std = 0.2 * (-torch.log((x-x.min()/(x.max() - x.min()))) + 9 )
 
 # Plot the model output and its derivative
-plot_model_output_and_derivative(model, x, y, y_derivative, std)
+fig, axs = plot_model_output_and_derivative(model, x, torch.exp(1e1*y), y_finite_diff, std)
+axs[0].set_ylim(1, axs[0].get_ylim()[1])
+axs[1].set_ylim(0, axs[1].get_ylim()[1])
+
+for ax in axs:
+    ax.set_xlim(5e-2, ax.get_xlim()[1])
+    # ax.tick_params(axis='both', which='both', length=0)
+    # ax.set_xticklabels([])
+    # ax.set_yticklabels([])
+    
+fig.savefig("conceptual_SAR.pdf", bbox_inches='tight', dpi=300)
