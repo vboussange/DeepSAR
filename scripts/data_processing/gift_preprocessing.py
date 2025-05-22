17225f7d38200f6393d82bc6fa9adc621363198a
Victor Boussange <vic.boussange@gmail.com>
2025-05-22 13:23:46 +0200

    updated gift preprocessing

--- scripts/data_processing/gift_preprocessing.py
+++ scripts/data_processing/gift_preprocessing.py
@@ -35,21 +35,32 @@ assert species_eva.issubset(species_gift), "Not all EVA species are present in G
 print("Cropping plot_gdf to the extent of climate_raster...")
 gift_plot_df = gift_plot_df.cx[EXTENT_DATASET[0]:EXTENT_DATASET[2], EXTENT_DATASET[1]:EXTENT_DATASET[3]]
 
+# saving habitat agnostic data
+output_path = OUTPUT_FOLDER / "unfiltered"
+output_path.mkdir(parents=True, exist_ok=True)
+gift_species_df.to_parquet(output_path / "species_data.parquet")
+gift_plot_df.to_file(output_path / "plot_data.gpkg", driver="GPKG")
+print(f"\nSaved GIFT habitat-agnostic data to {output_path / 'species_data.parquet'} and {output_path / 'plot_data.gpkg'}")
+
 # creating habitat specific plots from GIFT data
 plot_id = 0
 filtered_gift_species_df = []
 filtered_gift_plot_df = []
-for hab in ["Q", "R", "S", "T", "all"]:
+for hab in ["all"]:
     # get all species in the habitat
     if hab == "all":
         eva_plot_id_hab = eva_plot_df[eva_plot_df["level_1"].isin(["Q", "R", "S", "T"])].plot_id.unique() #this should be equal to eva_plot_df.plot_id.unique()
     else:
         eva_plot_id_hab = eva_plot_df[eva_plot_df["level_1"] == hab].plot_id.unique()
+    # Filter out from the GIFT data all species that are not in the EVA data
     hab_species = eva_species_df[eva_species_df["plot_id"].isin(eva_plot_id_hab)].gift_matched_species_name.unique()
     gift_sp_df = gift_species_df[gift_species_df["work_species_cleaned"].isin(hab_species)]
+    # Creating new plot IDs
     gift_old_plot_ids = gift_sp_df["entity_ID"].dropna().unique()
     gift_new_plot_ids = {old_id: plot_id+i for (i, old_id) in enumerate(gift_old_plot_ids)}
+    # assigning new plot IDs to the species data
     gift_sp_df["entity_ID"] = gift_sp_df["entity_ID"].map(gift_new_plot_ids)
+    # Removing plots which do not have any species from the EVA data, and assigning new plot IDs
     gift_pl_df = gift_plot_df[gift_plot_df["entity_ID"].isin(gift_old_plot_ids)]
     gift_pl_df["entity_ID"] = gift_pl_df["entity_ID"].map(gift_new_plot_ids)
     gift_pl_df["level_1"] = hab
@@ -63,7 +74,8 @@ gift_plot_df = pd.concat(filtered_gift_plot_df, ignore_index=True)
 assert len(gift_plot_df) == len(gift_plot_df["entity_ID"].unique())
 assert set(gift_plot_df.entity_ID).issubset(set(gift_species_df.entity_ID)), " plot_gdf.entity_ID is not a subset of gift_species_df.entity_ID"
 
-
-gift_species_df.to_parquet(OUTPUT_FOLDER / "species_data.parquet")
-gift_plot_df.to_file(OUTPUT_FOLDER / "plot_data.gpkg", driver="GPKG")
-print(f"\nSaved preprocessed data to {OUTPUT_FOLDER / 'species_data.parquet'} and {OUTPUT_FOLDER / 'plot_data.gpkg'}")
+output_path = OUTPUT_FOLDER / "filtered"
+output_path.mkdir(parents=True, exist_ok=True)
+gift_species_df.to_parquet(output_path / "species_data.parquet")
+gift_plot_df.to_file(output_path / "plot_data.gpkg", driver="GPKG")
+print(f"\nSaved GIFT habitat-specific data to {output_path / 'species_data.parquet'} and {output_path / 'plot_data.gpkg'}")
